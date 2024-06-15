380a7111d473934885dc201d0d64d7d1006ef85e
alex-jimsh <jimshiashvilia@gmail.com>
2024-06-15 05:21:21 +0400

    first commit

---
+++ day 1/lesson2.py
@@ -0,0 +1,9 @@
+name = "aleksandre"
+surname = "jimshiahsvili"
+age = "16"
+height = "1.7"
+
+print(f"hello my name is {name} {surname}. i am {age} years old and stand up to {height} tall.") #N1 The first variation of how it can be done
+
+print("hello my name is " + name + " " + surname + "." + " i am " + age + " years old and stand up to " + height + " tall.")# N2 Here is the second one
+
